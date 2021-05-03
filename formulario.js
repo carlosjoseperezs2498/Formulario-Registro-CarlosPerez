cf68b461f6174510adab401c889f2baba3aeb289
carlosjoseperezs2498 <83595971+carlosjoseperezs2498@users.noreply.github.com>
2021-05-03 10:05:37 -0600

    Add files via upload

--- formulario.js
+++ formulario.js
@@ -82,7 +82,7 @@ function registrarse(){
     console.log("entro");
     let parameters = $("#formulario").serialize();
     console.log(parameters);
-    /*$.ajax({
+    $.ajax({
         url:"ajax/form.php?accion=SetData",
         method:"POST",
         data: parameters,
@@ -105,7 +105,7 @@ function registrarse(){
         error:function(answer){
             console.log("Error");
         }
-    });*/
+    });
 }
 
 $(document).ready(function(){
